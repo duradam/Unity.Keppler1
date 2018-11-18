8d5fc046f10fef8b8f92993ee368bce7c403e5bf
duradam <40144116+duradam@users.noreply.github.com>
2018-11-18 18:41:50 +0100

    Changed from cubes to spheres; added textures

---
+++ Keppler1/Assets/Scripts/AddVelocity.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddVelocity : MonoBehaviour {
+
+	public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb.AddForce(new Vector3(.1f,5,.5f), ForceMode.Impulse);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
